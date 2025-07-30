6fe19619b1a07881a80a9bf90dab6f9cca1e9fb0
John McAleely <john@mcaleely.com>
2025-07-30 11:35:03 +0100

    add button.ya example, and update gpio.ya to avoid 'pin'

--- tools/build-specimen.sh
+++ tools/build-specimen.sh
@@ -7,17 +7,19 @@ then
 fi
 
 ../../bin/hostyarg format -fs ../../build/specimen.uf2
+../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add gpio.ya
+../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add irq.ya
+../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add machine.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add alarm.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add blinky.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add coffee.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add coroutine-flash.ya
-../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add gpio.ya
-../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add machine.ya
-../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add main.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add multicore-flash.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add scone.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add timed-flash.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add ws2812.ya
+../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add button.ya
+../../bin/hostyarg addfile -fs ../../build/specimen.uf2 -add main.ya
 
 popd > /dev/null
 
