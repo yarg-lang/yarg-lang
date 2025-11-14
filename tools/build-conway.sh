eeb69a353f506c221b9c3bfb50bfc199482443ec
John McAleely <john@mcaleely.com>
2025-11-14 16:06:26 +0000

    conway updates for new ws2812 lib

--- tools/build-conway.sh
+++ tools/build-conway.sh
@@ -11,7 +11,6 @@ then
 fi
 
 cp ../../../build/yarg-lang.uf2 $TARGETUF2
-$HOSTYARG format -fs $TARGETUF2
 $HOSTYARG addfile -fs $TARGETUF2 -add conway.ya
 $HOSTYARG addfile -fs $TARGETUF2 -add cube_bit.ya
 $HOSTYARG addfile -fs $TARGETUF2 -add main.ya
