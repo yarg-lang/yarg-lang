ea03cf5449d0bf55bb060311b10ed8649b3077cb
John McAleely <john@mcaleely.com>
2025-11-17 11:24:36 +0000

    Update from 0.1.0 to 0.2.0

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TARGETUF2=build/yarg-lang-pico-0.1.0.uf2
+TARGETUF2=build/yarg-lang-pico-0.2.0.uf2
 
-if [ -e release/yarg-lang-pico-0.1.0.tgz ]
+if [ -e release/yarg-lang-pico-0.2.0.tgz ]
 then
-    rm release/yarg-lang-pico-0.1.0.tgz
+    rm release/yarg-lang-pico-0.2.0.tgz
 fi
 
 if [ -e $TARGETUF2 ]
@@ -25,17 +25,20 @@ cp build/yarg-lang.uf2 $TARGETUF2
 
 
 pushd yarg/specimen
-../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add alarm.ya
-../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add blinky.ya
 ../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add hello_led.ya
 ../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add hello_button.ya
+../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add alarm.ya
+../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add blinky.ya
+../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add button.ya
+../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add button_flash.ya
 ../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add cheese.ya
 ../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add coroutine-flash.ya
 ../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add multicore-flash.ya
 ../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add scone.ya
+../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add serial-echo.ya
+../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add serial-input.ya
 ../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add timed-flash.ya
 ../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add ws2812.ya
-../../bin/hostyarg addfile -fs ../../$TARGETUF2 -add button.ya
 popd
 
 if [ ! -e release ]
@@ -45,8 +48,8 @@ fi
 
 cp $TARGETUF2 build/release
 cp docs/release_README build/release/README
-cp build/yarg-lang-0.1.0.vsix build/release/extras/
+cp build/yarg-lang-0.2.0.vsix build/release/extras/
 
 pushd build/release
-tar -cvzf ../../release/yarg-lang-pico-0.1.0.tgz .
+tar -cvzf ../../release/yarg-lang-pico-0.2.0.tgz .
 popd
