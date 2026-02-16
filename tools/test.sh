8e695505040cb71be4b2baa5fbd5f96d9944cccf
John McAleely <john@mcaleely.com>
2026-02-16 14:37:48 +0000

    bigint bc tests in separate suite

--- tools/test.sh
+++ tools/test.sh
@@ -8,4 +8,6 @@ popd >/dev/null
 
 ./bin/hostyarg runtests -tests "yarg/test" -interpreter "bin/cyarg" || TEST_ERROR=1
 
+./bin/cyarg test/bc/int.ya | bc -lLS 0 | diff - test/bc/int.expected || TEST_ERROR=1
+
 exit $TEST_ERROR
