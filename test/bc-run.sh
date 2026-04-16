cf005861451dfc34133890dfce1d38da7fd96b5f
John McAleely <john@mcaleely.com>
2026-04-16 10:21:18 +0100

    order makes test/bc-run.sh more readable

--- test/bc-run.sh
+++ test/bc-run.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
 REPETITIONS=${1:-200}
 SEED=${2:-$RANDOM}
-
-if [ ! -d "build" ]
-then
-    mkdir build
-fi
-
-echo "${0} REPETITIONS=$REPETITIONS, SEED=$SEED"
-echo "REPETITIONS=$REPETITIONS, SEED=$SEED" >> build/bc-seeds.txt
 
 if ! [[ "$REPETITIONS" =~ ^[1-9][0-9]*$ ]]; then
   echo "usage: $0 [repetitions]" >&2
@@ -20,6 +11,14 @@ if ! [[ "$SEED" =~ ^[0-9]+$ ]]; then
   echo "usage: $0 [repetitions] [seed]" >&2
   exit 2
 fi
+
+if [ ! -d "build" ]
+then
+    mkdir build
+fi
+
+echo "${0} REPETITIONS=$REPETITIONS, SEED=$SEED"
+echo "REPETITIONS=$REPETITIONS, SEED=$SEED" >> build/bc-seeds.txt
 
 RESULT=0
 ./bin/cyarg --lib yarg/specimen test/bc/int.ya -- "$SEED" "$REPETITIONS" | bc -lLS 0 | diff - test/bc/int.expected || RESULT=$?
