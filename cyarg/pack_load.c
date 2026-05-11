e657f88316bef7faac834d8d821bfbf91a27eee3
John McAleely <john@mcaleely.com>
2026-05-11 15:41:51 +0100

    make packageFileHeader a pointer

--- cyarg/pack_load.c
+++ cyarg/pack_load.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 #include "pack_format.h"
 
@@ -22,25 +23,27 @@ struct ObjFunction *loadPackage(char const *path) {
     FILE *file = fopen(path, "rb");
     if (file == 0) return 0;
 
-    PackageFileHeader h = {0};
-    assert(sizeof h == 24);
+    PackageFileHeader buffer;
+    memset(&buffer, 0, sizeof buffer);
+    PackageFileHeader* h = &buffer;
+    assert(sizeof *h == 24);
 
-    size_t n = fread(&h, sizeof h, 1, file);
+    size_t n = fread(h, sizeof *h, 1, file);
     assert(n == 1);
 
-    if (memcmp(h.magic_, magic, PACKAGE_MAGIC_LEN) != 0) {
+    if (memcmp(h->magic_, magic, PACKAGE_MAGIC_LEN) != 0) {
         r = EX_PROTOCOL;
     }
-    if (h.version_ != version) {
+    if (h->version_ != version) {
         r = EX_DATAERR;
         goto exit;
     }
 
-    uint8_t* const body = realloc(0, h.bodyLength_);
+    uint8_t* const body = realloc(0, h->bodyLength_);
     assert((long)body % 8 == 0);
 
-    size_t bodyLen = fread(body, 1, h.bodyLength_, file);
-    assert(bodyLen == h.bodyLength_);
+    size_t bodyLen = fread(body, 1, h->bodyLength_, file);
+    assert(bodyLen == h->bodyLength_);
 
     double *doubles = (double *)body;
 
@@ -57,39 +60,39 @@ struct ObjFunction *loadPackage(char const *path) {
     } PackedChunk;
     PackedChunk const *chunks[256];
 
-    uint8_t const *next = body + h.numDoubles_ * sizeof (double);
+    uint8_t const *next = body + h->numDoubles_ * sizeof (double);
 
     DP(chunks__ = (uint32_t)(next - body));
-    for (int i = 0; i < h.numChunks_; i++) {
+    for (int i = 0; i < h->numChunks_; i++) {
         chunks[i] = (PackedChunk const *)next;
         next += 8 + 4 * chunks[i]->numConsts_;
     }
     uint8_t const *intFile = next;
 
     DP(ints__ = (uint32_t)(next - body));
-    for (int i = 0; i < h.numInts_; i++) {
+    for (int i = 0; i < h->numInts_; i++) {
         Int const *thisInt = (Int const *)next;
         next += sizeof (Int) + sizeof (uint16_t) * thisInt->m_;
     }
     uint8_t const *stringFile = next;
 
     DP(strings__ = (uint32_t)(next - body));
-    for (int i = 0; i < h.numStrings_; i++) {
+    for (int i = 0; i < h->numStrings_; i++) {
         char const *thisString = (char const *)next;
         next += strlen(thisString) + 1;
         DP(printf("%s, %u, %u\n", thisString, (uint32_t)((next - body) - strings__), (uint32_t)(next - body)));
     }
 
     ObjFunction *currentFunction;
-    functions = realloc(0, h.numChunks_ * sizeof (ObjFunction *));
+    functions = realloc(0, h->numChunks_ * sizeof (ObjFunction *));
 
-    for (int i = 0; i < h.numChunks_; i++) {
+    for (int i = 0; i < h->numChunks_; i++) {
         functions[i] = newFunction();
     }
 
     uint8_t const *startOfCode = next;
     DP(code__ = (uint32_t)(next - body));
-    for (int i = 0; i < h.numChunks_; i++) {
+    for (int i = 0; i < h->numChunks_; i++) {
         currentFunction = functions[i];
         initDynamicValueArray(&currentFunction->chunk.constants);
         currentFunction->chunk.xip = true;
@@ -102,11 +105,11 @@ struct ObjFunction *loadPackage(char const *path) {
 
     DP(lines__ = (uint32_t)(next - body));
     DP(printf("line, offset\n"));
-    for (int i = 0; i < h.numLines_; i++) {
+    for (int i = 0; i < h->numLines_; i++) {
         int offset = 0;
         memcpy(&offset, next, 3);
         if (offset != 0xffffff) {
-            for (int c = 0; c < h.numChunks_; c++) {
+            for (int c = 0; c < h->numChunks_; c++) {
                 ObjFunction *cf = functions[c];
                 size_t start = cf->chunk.code - startOfCode;
                 size_t end = start + cf->chunk.count;
@@ -127,16 +130,16 @@ struct ObjFunction *loadPackage(char const *path) {
     }
 
     DP(names__ = (uint32_t)(next - body));
-    if (h.numLines_ > 0) {
-        for (int i = 0; i < h.numChunks_; i++) {
+    if (h->numLines_ > 0) {
+        for (int i = 0; i < h->numChunks_; i++) {
             functions[i]->fName = copyString((char *)next, (int)strlen((char *)next)); // todo - mark as xip
             next += 16;
         }
     }
 
     DP(end__ = (uint32_t)(next - body));
-    assert(h.numChunks_ >= 1);
-    for (int i = 0; i < h.numChunks_; i++) {
+    assert(h->numChunks_ >= 1);
+    for (int i = 0; i < h->numChunks_; i++) {
         currentFunction = functions[i];
         DP(printf("Chunk:%d code@%p numConsts:%d\n", i, currentFunction->chunk.code, (int)chunks[i]->numConsts_));
         for (int c = 0; c < chunks[i]->numConsts_; c++) {
