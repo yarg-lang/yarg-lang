0ba1348fb265757e4590ea02b7a864a5076e46f5
John McAleely <john@mcaleely.com>
2026-05-12 14:25:40 +0100

    migrate test-hardware to use compiled scripts

--- tools/build-test-hardware.sh
+++ tools/build-test-hardware.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-HOSTYARG="../../bin/yarg"
-TARGETUF2="../../build/test-hardware.uf2"
+HOSTYARG="bin/yarg"
+TARGETUF2="build/test-hardware.uf2"
 
 
-pushd test/hardware > /dev/null
+BUILD_DIR=build/test-hardware
+
 if [ -e $TARGETUF2 ]
 then
     rm $TARGETUF2
 fi
+mkdir -p "$BUILD_DIR"
 
-cp ../../build/yarg-lang.uf2 $TARGETUF2
-for test in cheese.ya scone.ya interrupt.ya alarm.ya blinky.ya main.ya coroutine-flash.ya multicore-flash.ya timed-flash.ya
+cp build/yarg-lang.uf2 $TARGETUF2
+
+$HOSTYARG cp -fs $TARGETUF2 -src "test/hardware/main.ya" -dest "main.ya"
+
+for test in cheese scone interrupt alarm blinky coroutine-flash multicore-flash timed-flash
 do
-    $HOSTYARG compile --interpreter ../../bin/cyarg --source $test
-    $HOSTYARG cp -fs $TARGETUF2 -src $test -dest $test
+    $HOSTYARG compile --interpreter bin/cyarg --source "test/hardware/$test.ya" --output "$BUILD_DIR/$test.yb"
+    $HOSTYARG cp -fs $TARGETUF2 -src "$BUILD_DIR/$test.yb" -dest "$test.yb"
 done
 
-$HOSTYARG compile --interpreter ../../bin/cyarg --source ../../yarg/specimen/hello_led.ya
-$HOSTYARG cp -fs $TARGETUF2 -src ../../yarg/specimen/hello_led.ya -dest hello_led.ya
-
-$HOSTYARG compile --interpreter ../../bin/cyarg --source ../../yarg/specimen/hello_button.ya
-$HOSTYARG cp -fs $TARGETUF2 -src ../../yarg/specimen/hello_button.ya -dest hello_button.ya
-popd > /dev/null
+for specimen in hello_led hello_button
+do
+    $HOSTYARG compile --interpreter bin/cyarg --source "yarg/specimen/$specimen.ya" --output "$BUILD_DIR/$specimen.yb"
+    $HOSTYARG cp -fs $TARGETUF2 -src "$BUILD_DIR/$specimen.yb" -dest "$specimen.yb"
+done
